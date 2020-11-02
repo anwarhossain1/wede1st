c376d8934d68a3f578fb9564976d9fe558d677fa
anwarhossain1 <irishkhan33@gmail.com>
2020-11-02 11:35:05 +0600

    new line add kora baki

---
+++ 1st-ques.js
@@ -0,0 +1,8 @@
+function customPrint(n, message) {
+    (n>0) ? console.log(message.repeat(n)): console.log("Input a value greater than zero") ;
+    }
+
+    customPrint(5,"Hello World!");
+    
+
+    //new line add kora baki
